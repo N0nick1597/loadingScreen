019db39af83a59c609cf07dbd2f5112fe5d4c9f5
N0nick <50700228+N0nick1597@users.noreply.github.com>
2021-08-23 23:21:47 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+files {
+    'music/music.mp3',
+    'index.html',
+    'images/20201018_131853.jpg',
+    'images/amenities-services-114244.png',
+    'images/logo.png',
+    'images/tenor-2.gif',
+    'images/wp3870461.jpg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
